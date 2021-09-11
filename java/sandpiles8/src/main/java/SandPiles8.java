b38c9a8f01117689370336a2f82981331ecdf883
bastihz <bastihz.dev@posteo.de>
2021-09-12 00:41:46 +0200

    Make 8 sided Sandpile much faster by using symmetry.

--- java/sandpiles8/src/main/java/SandPiles8.java
+++ java/sandpiles8/src/main/java/SandPiles8.java
@@ -3,12 +3,11 @@
 
 import processing.core.PApplet;
 
-import java.util.Arrays;
-
 public class SandPiles8 extends PApplet {
 
-    private static final int N_SAND = 250_000;
+    private static final int N_SAND = 500_000;
     private static final int SQUARE_SIZE = 2;
+    private static final int SQUARE_SIZE_TWICE = SQUARE_SIZE * 2;
     private static int defaultWidth = 800;
     private static int defaultHeight = 800;
     private static boolean animate = true;
@@ -29,7 +28,9 @@ public class SandPiles8 extends PApplet {
     private int[] nextPile;
     private int pileWidthMinus1;
     private int pileWidthPlus1;
-    private int pileHeightPlus1;
+    private int pileHeightMinus1;
+    private int pileWidthMinus2;
+    private int pileHeightMinus2;
 
     public static void main(final String[] args) {
         if (args.length == 2) {
@@ -51,13 +52,15 @@ public class SandPiles8 extends PApplet {
             noLoop();
         }
         noStroke();
-        pileWidth = (int) Math.ceil((double) width / SQUARE_SIZE);
-        pileHeight = (int) Math.ceil((double) height / SQUARE_SIZE);
+        pileWidth = width / SQUARE_SIZE_TWICE;
+        pileHeight = height / SQUARE_SIZE_TWICE;
         pileWidthMinus1 = pileWidth - 1;
         pileWidthPlus1 = pileWidth + 1;
-        pileHeightPlus1 = pileHeight + 1;
+        pileHeightMinus1 = pileHeight - 1;
+        pileWidthMinus2 = pileWidth - 2;
+        pileHeightMinus2 = pileHeight - 2;
         pile = new int[pileWidth * pileHeight];
-        pile[pile.length / 2 - pileWidth / 2 - 1] = N_SAND;
+        pile[pile.length - 1] = N_SAND;
         nextPile = pile.clone();
     }
 
@@ -76,12 +79,16 @@ public class SandPiles8 extends PApplet {
 
         for (int x = 0; x < pileWidth; x++) {
             for (int y = 0; y < pileHeight; y++) {
-                final int i = xyToPileIndex(x, y);
-                final int n = pile[i];
+                final int n = pile[xyToPileIndex(x, y)];
                 fill(n < 8 ? colors[n] : color(255));
                 final int squareX = x * SQUARE_SIZE;
                 final int squareY = y * SQUARE_SIZE;
-                square(squareX, squareY, SQUARE_SIZE);
+                final int rightX = width - squareX - SQUARE_SIZE_TWICE;
+                final int bottomY = height - squareY - SQUARE_SIZE_TWICE;
+                square(squareX, squareY, SQUARE_SIZE);  // top left quadrant
+                square(rightX, squareY, SQUARE_SIZE);  // top right quadrant
+                square(squareX, bottomY, SQUARE_SIZE);  // bottom left quadrant
+                square(rightX, bottomY, SQUARE_SIZE);  // bottom right quadrant
             }
         }
     }
@@ -92,8 +99,6 @@ public class SandPiles8 extends PApplet {
             animateFast = !animateFast;
         } else if (key == RETURN || key == ENTER) {
             animate = false;
-        } else if (key == 'p') {  // DEBUG
-            System.out.println(Arrays.stream(pile).sum());
         }
     }
 
@@ -110,40 +115,65 @@ public class SandPiles8 extends PApplet {
                 if (n >= 8) {
                     notFinished = true;
                     final int nToDistribute = n / 8;
+                    final int nToDistributeTwice = nToDistribute + nToDistribute;
                     nextPile[i] -= n - n % 8;
 
                     final boolean leftOk = x > 0;
                     final boolean topOk = y > 0;
                     final boolean rightOk = x < pileWidthMinus1;
-                    final boolean bottomOk = y < pileHeightPlus1;
+                    final boolean bottomOk = y < pileHeightMinus1;
                     final boolean topLeftOk = topOk && leftOk;
                     final boolean topRightOk = topOk && rightOk;
                     final boolean bottomLeftOk = bottomOk && leftOk;
                     final boolean bottomRightOk = bottomOk && rightOk;
+                    final boolean bleedsToTheRight = x == pileWidthMinus2;
+                    final boolean bleedsToTheBottom = y == pileHeightMinus2;
 
                     if (leftOk) {
                         nextPile[i - 1] += nToDistribute;
                     }
+                    if (topLeftOk) {
+                        nextPile[i - pileWidthPlus1] += nToDistribute;
+                    }
                     if (topOk) {
                         nextPile[i - pileWidth] += nToDistribute;
                     }
-                    if (rightOk) {
-                        nextPile[i + 1] += nToDistribute;
-                    }
-                    if (bottomOk) {
-                        nextPile[i + pileWidth] += nToDistribute;
-                    }
-                    if (topLeftOk) {
-                        nextPile[i - pileWidthMinus1] += nToDistribute;
-                    }
                     if (topRightOk) {
-                        nextPile[i - pileWidthPlus1] += nToDistribute;
+                        if (bleedsToTheRight) {
+                            nextPile[i - pileWidthMinus1] += nToDistributeTwice;
+                        } else {
+                            nextPile[i - pileWidthMinus1] += nToDistribute;
+                        }
                     }
-                    if (bottomLeftOk) {
-                        nextPile[i + pileWidthMinus1] += nToDistribute;
+                    if (rightOk) {
+                        if (bleedsToTheRight) {
+                            nextPile[i + 1] += nToDistributeTwice;
+                        } else {
+                            nextPile[i + 1] += nToDistribute;
+                        }
                     }
                     if (bottomRightOk) {
-                        nextPile[i + pileWidthPlus1] += nToDistribute;
+                        if (bleedsToTheRight && bleedsToTheBottom) {
+                            nextPile[i + pileWidthPlus1] += nToDistribute * 4;
+                        } else if (bleedsToTheRight || bleedsToTheBottom) {
+                            nextPile[i + pileWidthPlus1] += nToDistributeTwice;
+                        } else {
+                            nextPile[i + pileWidthPlus1] += nToDistribute;
+                        }
+                    }
+                    if (bottomOk) {
+                        if (bleedsToTheBottom) {
+                            nextPile[i + pileWidth] += nToDistributeTwice;
+                        } else {
+                            nextPile[i + pileWidth] += nToDistribute;
+                        }
+                    }
+                    if (bottomLeftOk) {
+                        if (bleedsToTheBottom) {
+                            nextPile[i + pileWidthMinus1] += nToDistributeTwice;
+                        } else {
+                            nextPile[i + pileWidthMinus1] += nToDistribute;
+                        }
                     }
                 }
             }
