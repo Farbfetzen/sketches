ff0cb3bdd0ddabc30f733c62dd33c90c7527381f
bastihz <bastihz.dev@posteo.de>
2021-09-26 11:53:58 +0200

    restructure project

--- template/src/main/java/Template.java
+++ template/src/main/java/Template.java
@@ -2,21 +2,14 @@ import processing.core.PApplet;
 
 public class Template extends PApplet {
 
-    private static int defaultWidth = 1200;
-    private static int defaultHeight = 800;
-
     public static void main(final String[] args) {
-        if (args.length == 2) {
-            defaultWidth = Integer.parseInt(args[0]);
-            defaultHeight = Integer.parseInt(args[1]);
-        }
         PApplet.main(Template.class.getName());
     }
 
     @Override
     public void settings() {
         // This method must be used for size(), smooth() and noSmooth().
-        size(defaultWidth, defaultHeight);
+        size(1200, 800);
     }
 
     @Override
